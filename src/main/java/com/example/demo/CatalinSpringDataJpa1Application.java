da5329dd45eae182c543e50ef9ec736bb5811558
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-14 19:27:56 -0300

    DB CONNECTED AND WORKING

--- src/main/java/com/example/demo/CatalinSpringDataJpa1Application.java
+++ src/main/java/com/example/demo/CatalinSpringDataJpa1Application.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.repositories.UserRepository;
+import com.example.demo.model.User;
 
 @SpringBootApplication
 public class CatalinSpringDataJpa1Application {
@@ -10,4 +18,18 @@ public class CatalinSpringDataJpa1Application {
 		SpringApplication.run(CatalinSpringDataJpa1Application.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner configure(UserRepository userRepository) {
+		return env ->
+		{
+			User user1 = new User("beth", LocalDate.of(2020, Month.AUGUST, 3));
+			User user2 = new User("mike", LocalDate.of(2020, Month.JANUARY, 18));
+
+			userRepository.save(user1);
+			userRepository.save(user2);
+
+			userRepository.findAll().forEach(System.out::println);
+		};
+	}
+	
 }
