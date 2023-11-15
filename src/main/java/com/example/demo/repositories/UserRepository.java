b3122bfdaf7025644786b3ce970d7bfe0524ec44
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-15 18:58:03 -0300

    building repository

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -8,12 +8,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.util.Streamable;
 
 import com.example.demo.model.Projection;
 import com.example.demo.model.User;
+
+import jakarta.transaction.Transactional;
 
 /**
  * We will test theses methods on the test package, allowing us to use JUnit
@@ -119,4 +122,24 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // dynamic return type
     <T> List<T> findByEmail(String username, Class<T> type);
     
+    
+    /**
+     * Modifying queries: importé para @Transactional el de Jakarta en lugar del de Spring, en el libro era al revés.
+     * 
+     * By combining @Modifying and @Transactional, you ensure that the modifying query is executed within a transaction, 
+     * and the transactional behavior, including commit or rollback, is handled appropriately. This is crucial to maintain 
+     * the consistency and integrity of your data when performing modifications to the database.
+     */
+    @Modifying
+    @Transactional
+    @Query("update User u set u.level = ?2 where u.level = ?1")
+    int updateLevel(int oldLevel, int newLevel);
+
+    @Transactional
+    int deleteByLevel(int level);
+
+    @Transactional
+    @Modifying
+    @Query("delete from User u where u.level = ?1")
+    int deleteBulkByLevel(int level);
 }
